ef58f4577f03fba8f036aeefe00eaabe1c0d6464
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-05-24 10:21:22 +0530

    basic express setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message : 'Hello World!'
+    });
+})
+
+app.post('/', (req, res) => {
+    res.send('Hello World! POST');
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+} )
