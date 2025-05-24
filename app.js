9f04295dbda01bea9f190ce458ae6283700648d8
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-05-24 12:52:27 +0530

    add new tour - post request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require("fs");
 
 const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -17,8 +18,19 @@ app.get('/api/v1/tours', (req, res) => {
     });
 })
 
-app.post('/', (req, res) => {
-    res.send('Hello World! POST');
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
 })
 
 const port = 3000;
