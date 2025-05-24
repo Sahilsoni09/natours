dd74d982ec9d859ad0bcf0c807cf7ba891229a9b
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-05-24 10:53:20 +0530

    get all tours

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const fs = require("fs");
 
 const app = express();
 
-app.get('/', (req, res) => {
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
-        message : 'Hello World!'
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours: tours
+        }
     });
 })
 
