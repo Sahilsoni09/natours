4abe842dda179905b4e48d5a7faec1fc29f5238c
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-05-27 05:51:54 +0530

    connecting mongoDB

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
-const { request } = require('http');
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev')); // Logging middleware for development
+}
+
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.static(`${__dirname}/public`)); // Serve static files from the public directory
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 })
-
-app.use(morgan('dev')); // Logging middleware
-
 
 
 app.use("/api/v1/tours", tourRouter);
--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app'); 
 
-const port = 3000;
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: false,
+}).then(() => {
+    console.log('DB connection successful!');
+})
+
+const port = process.env.PORT ||3000;
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
